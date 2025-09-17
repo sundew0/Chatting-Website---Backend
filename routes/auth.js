165315e4962b6f1a3ac796a8a86eadceb95ecb11
mads <madssunshine10@gmail.com>
2025-09-17 20:41:11 +1000

    add old loggin function - to be changed

--- routes/auth.js
+++ routes/auth.js
@@ -28,5 +28,34 @@ router.post(
     res.status(201).send();
   }
 );
+router.post("/login_user", 
+  [
+    body("username").isAlphanumeric().isLength({ min: 3, max: 20 }),
+    body("password").isLength({ min: 8 })
+  ],
+  async (req, res) => {
+    console.log("BODY RECEIVED:", req.body);
+    const { username, password } = req.body;
+
+    let users = []
+  
+    users = JSON.parse(fs.readFileSync(USERS_FILE, "utf8"));
+    const user =  users.find(u => u.username.toLowerCase() === username.toLowerCase())
+    if (user) {
+
+      const match = await bcrypt.compare(password, user.password)
+      const whitelisted = ["sundew", "sewil", "areila", "arelia"]
+      if (match) {
+
+//        if (whitelisted.includes(username.toLowerCase()))
+        return res.status(200).json( { passed: true})
+      }
+    }
+      
+      return res.status(401).json({ error: "Invalid username or password" });
+
+      
+
+})
 
 module.exports = router;
